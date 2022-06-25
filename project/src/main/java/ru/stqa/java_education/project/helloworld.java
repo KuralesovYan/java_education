38f0236c918fc5ba1a2611ec597f50b505aec592
KuralesovYan <107788091+KuralesovYan@users.noreply.github.com>
2022-06-25 22:36:53 +0300

    Добавлены несколько функций "Мозговзрыв"

--- project/src/main/java/ru/stqa/java_education/project/helloworld.java
+++ project/src/main/java/ru/stqa/java_education/project/helloworld.java
@@ -1,8 +1,31 @@
 package ru.stqa.java_education.project;
 
-public class helloworld {
+import java.awt.desktop.AboutEvent;
 
+public class helloworld {
 	public static void main(String[] args) {
-System.out.println("Not_war_ukrain");
+		hello(" and RASHA");
+
+		double lena = 4.0;
+		System.out.println(area (lena));
+
+		double a= 5;
+		double b= 6;
+		System.out.println("какой-то текст " + a + " умножить " + b + " равно " + area(a,b));
+	}
+	public static void hello(String A) {
+		System.out.println("Not war Ukrain" + A);
+	}
+	public static double area (double l) {
+		return l * l;
+	}
+	public static double area (double a, double b) {
+		return a * b;
 	}
 }
+	//String A = "Заебал, твой тип, не правильный тип, держи свои ";
+		//System.out.println(A+"психические проблемы при себе");
+
+				//double l = 6;
+				//double s = l*l;
+				//System.out.println("Площадь " + l + " = " + s);
